4d13724324b62dd59cdba18580c7698bf6cc736e
Vladimir Panov <vladimir.panov@phystech.edu>
2016-10-18 20:27:11 +0300

    Исправил Views

--- core/views.py
+++ core/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import RedirectView
+
+
+class HomePageView(RedirectView):
+    permanent = False
+
+    def get_redirect_url(self, *args, **kwargs):
+        return "/timetable"
--- study_calendar/views.py
+++ study_calendar/views.py
@@ -1,41 +1,25 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import TemplateView
-from django.views.generic import RedirectView
+from django.views.generic import ListView
 from django.views.generic import DetailView
 
 from study_calendar.models.TimeTable import TimeTable
 from study_calendar.models.Cell import Cell
 
 
-class HomePageView(RedirectView):
-    permanent = True
-
-    def get_redirect_url(self, *args, **kwargs):
-        return "/timetable"
-
-
 class TimeTableView(DetailView):
     template_name = "TimeTable.html"
     model = TimeTable
 
-    def get(self, request, *args, **kwargs):
-        if 'timetable' in kwargs:
-            key = kwargs["timetable"]
-        else:
-            key = 1
-        return render(request, self.template_name, {'timetable': TimeTable.objects.get(pk=key)})
+    pk_url_kwarg = "timetable"
+    context_object_name = "timetable"
 
-class TimeTableListView(TemplateView):
+
+class TimeTableListView(ListView):
     template_name = "TimeTableList.html"
-
-    def get(self, request, *args, **kwargs):
-        return render(request, self.template_name, {'timetables': TimeTable.objects.all()})
-
-
-class CellView(TemplateView):
-    template_name = "Cell.html"
+    model = TimeTable
+    context_object_name = 'timetables'
 
 
 class CellDetailView(DetailView):
