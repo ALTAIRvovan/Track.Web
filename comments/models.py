066fd0c57959e9c0b4ed5b7c18a7bfc2a191d263
Vladimir Panov <vladimir.panov@phystech.edu>
2016-10-18 17:57:00 +0300

    Делал DetailView для Cells

--- comments/models.py
+++ comments/models.py
@@ -1,3 +1,14 @@
+from django.conf import settings
 from django.db import models
 
+
 # Create your models here.
+
+
+class Comment(models.Model):
+    text = models.TextField(default="text")
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    cell = models.ForeignKey('study_calendar.Cell')
+
+    def __str__(self):
+        return str(self.cell) + " " + str(self.author)
