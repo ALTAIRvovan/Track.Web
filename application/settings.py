cc3ea596a65ad6227bcde85f03d6971232d05980
Vladimir Panov <vladimir.panov@phystech.edu>
2016-12-12 21:32:58 +0300

    memcache

--- application/settings.py
+++ application/settings.py
@@ -129,3 +129,9 @@ INTERNAL_IPS = (
     '127.0.0.1'
 )
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+        'LOCATION': '127.0.0.1:11211',
+    }
+}
