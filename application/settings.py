b61fa3037e9bb3a90f198725a25dc7e1b4ca4bc2
Vladimir Panov <vladimir.panov@phystech.edu>
2016-12-28 14:46:52 +0300

    Переводы

--- application/settings.py
+++ application/settings.py
@@ -48,9 +48,28 @@ INSTALLED_APPS = [
 
 AUTH_USER_MODEL = 'core.User'
 
+# Internationalization
+# https://docs.djangoproject.com/en/1.10/topics/i18n/
+
+LANGUAGE_CODE = 'ru'
+
+LANGUAGES = (
+    ('ru', 'Russian'),
+    ('en', 'English'),
+)
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -110,19 +129,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
-# Internationalization
-# https://docs.djangoproject.com/en/1.10/topics/i18n/
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_L10N = True
-
-USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
@@ -189,3 +195,7 @@ LOGGING = {
         }
     },
 }
+
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, "locale"),
+)
--- application/urls.py
+++ application/urls.py
@@ -17,18 +17,24 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.i18n import i18n_patterns
 
 from core.views import HomePageView
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     url(r'^$', HomePageView.as_view(), name="root"),
     url(r'^auth/', include('core.urls', namespace="auth")),
     url(r'^admin/', admin.site.urls),
     url(r'^timetable/', include('study_calendar.urls', namespace="calendar")),
+)
+
+urlpatterns += [
+    url(r'^i18n/', include('django.conf.urls.i18n')),
 ]
 
 if settings.DEBUG:
     import debug_toolbar
+
     urlpatterns += [
         url(r'^__debug__/', include(debug_toolbar.urls)),
     ]
